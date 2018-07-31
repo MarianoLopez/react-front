2eed1dbce9df59fba788f0525c054cba3c0a5e23
Mariano Lopez <m_villa@hotmail.com>
2018-07-31 12:06:34 -0300

    sidebar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,133 @@
 import React, {Component} from 'react';
 
+import './css/Navbar.css'
+import $ from 'jquery'
+
+
 class MyComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isClose:true
+        }
+    }
+
+    hamburger_cross = () => {
+        let trigger = document.getElementsByClassName("hamburger")[0];
+        let overlay = document.getElementsByClassName("overlay")[0];
+        if(this.state.isClose){
+            overlay.style.display = 'none';
+            trigger.classList.remove('is-open');
+            trigger.classList.add('is-closed');
+            this.setState({
+                isClose:false
+            })
+        }else{
+            overlay.style.display = 'block';
+            trigger.classList.remove('is-closed');
+            trigger.classList.add('is-open');
+            this.setState({
+                isClose:true
+            })
+        }
+    };
+
+    componentDidMount(){
+        $(document).ready(function () {
+            var trigger = $('.hamburger'),
+                overlay = $('.overlay'),
+                isClosed = false;
+
+            trigger.click(function () {
+                hamburger_cross();
+            });
+
+            function hamburger_cross() {
+
+                if (isClosed == true) {
+                    overlay.hide();
+                    trigger.removeClass('is-open');
+                    trigger.addClass('is-closed');
+                    isClosed = false;
+                } else {
+                    overlay.show();
+                    trigger.removeClass('is-closed');
+                    trigger.addClass('is-open');
+                    isClosed = true;
+                }
+            }
+            $('[data-toggle="offcanvas"]').click(function () {
+                $('#wrapper').toggleClass('toggled');
+            });
+        });
+    }
+
     render() {
         return (
-            <div>
-                <h2>hello fucking world</h2>
+            <div id="wrapper">
+                <div className="overlay"></div>
+                <nav className="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+                    <ul className="nav sidebar-nav">
+                        <li className="sidebar-brand">
+                            <a href="#">
+                                Brand
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Events</a>
+                        </li>
+                        <li>
+                            <a href="#">Team</a>
+                        </li>
+                        <li className="dropdown">
+                            <a href="#" className="dropdown-toggle" data-toggle="dropdown">Works <span className="caret"></span></a>
+                            <ul className="dropdown-menu" role="menu">
+                                <li className="dropdown-header">Dropdown heading</li>
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div id="page-content-wrapper">
+                    <button type="button" className="hamburger is-closed" data-toggle="offcanvas"
+                            /*onClick={this.hamburger_cross}*/
+                    >
+                        <span className="hamb-top"></span>
+                        <span className="hamb-middle"></span>
+                        <span className="hamb-bottom"></span>
+                    </button>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-8 col-lg-offset-2">
+                                <h1>Fancy Toggle Sidebar Navigation</h1>
+                                <p>Bacon ipsum dolor sit amet tri-tip shoulder tenderloin shankle. Bresaola tail pancetta ball tip doner meatloaf corned beef. Kevin pastrami tri-tip prosciutto ham hock pork belly bacon pork loin salami pork chop shank corned beef tenderloin meatball cow. Pork bresaola meatloaf tongue, landjaeger tail andouille strip steak tenderloin sausage chicken tri-tip. Pastrami tri-tip kielbasa sausage porchetta pig sirloin boudin rump meatball andouille chuck tenderloin biltong shank </p>
+                                <p>Pig meatloaf bresaola, spare ribs venison short loin rump pork loin drumstick jowl meatball brisket. Landjaeger chicken fatback pork loin doner sirloin cow short ribs hamburger shoulder salami pastrami. Pork swine beef ribs t-bone flank filet mignon, ground round tongue. Tri-tip cow turducken shank beef shoulder bresaola tongue flank leberkas ball tip.</p>
+                                <p>Filet mignon brisket pancetta fatback short ribs short loin prosciutto jowl turducken biltong kevin pork chop pork beef ribs bresaola. Tongue beef ribs pastrami boudin. Chicken bresaola kielbasa strip steak biltong. Corned beef pork loin cow pig short ribs boudin bacon pork belly chicken andouille. Filet mignon flank turkey tongue. Turkey ball tip kielbasa pastrami flank tri-tip t-bone kevin landjaeger capicola tail fatback pork loin beef jerky.</p>
+                                <p>Chicken ham hock shankle, strip steak ground round meatball pork belly jowl pancetta sausage spare ribs. Pork loin cow salami pork belly. Tri-tip pork loin sausage jerky prosciutto t-bone bresaola frankfurter sirloin pork chop ribeye corned beef chuck. Short loin hamburger tenderloin, landjaeger venison porchetta strip steak turducken pancetta beef cow leberkas sausage beef ribs. Shoulder ham jerky kielbasa. Pig doner short loin pork chop. Short ribs frankfurter rump meatloaf.</p>
+                                <p>Filet mignon biltong chuck pork belly, corned beef ground round ribeye short loin rump swine. Hamburger drumstick turkey, shank rump biltong pork loin jowl sausage chicken. Rump pork belly fatback ball tip swine doner pig. Salami jerky cow, boudin pork chop sausage tongue andouille turkey.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         );
     }
